5a2f7262705201b697850c51d8488f32f681f53d
LRiceCooker <d.mathys@proton.me>
2024-09-07 00:45:01 +0200

    fix: wording + repo path

--- install.sh
+++ install.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
 # Variables
-GITHUB_REPO_URL="https://github.com/ton-user/ton-repo/archive/refs/heads/main.zip"
+GITHUB_REPO_URL="https://github.com/LRiceCooker/cornflake/archive/refs/heads/main.zip"
 TARGET_DIR="/mnt/nixos-config"
 HARDWARE_CONFIG="/etc/nixos/hardware-configuration.nix"
-FOLDER_NAME="ton-repo-main"
+FOLDER_NAME="cornflake"
 FLAKE_PATH="$TARGET_DIR/$FOLDER_NAME"
 
 function pull_github_repo() {
-    echo "Téléchargement du dépôt GitHub..."
+    echo "Pulling $GITHUB_REPO_URL..."
     curl -L $GITHUB_REPO_URL -o /tmp/nixos-config.zip
     
-    echo "Décompression du dépôt..."
+    echo "Extracting $GITHUB_REPO_URL..."
     unzip /tmp/nixos-config.zip -d /mnt
 }
 
 function generate_hardware_config() {
     if [ ! -f "$HARDWARE_CONFIG" ]; then
-        echo "Génération de la configuration matérielle..."
+        echo "generate hardware-configuration.nix..."
         nixos-generate-config --root /mnt
     fi
 }
 
 function replace_hardware_config() {
-    echo "Remplacement du fichier hardware-configuration.nix..."
+    echo "replace hardware-configuration.nix..."
     cp $HARDWARE_CONFIG $FLAKE_PATH/nixos/hardware-configuration.nix
 }
 
 function apply_flake() {
-    echo "Application du flake..."
+    echo "apply flake..."
     sudo nixos-rebuild switch --flake $FLAKE_PATH#$(cornflake)
 }
 
 function cleanup_old_generations() {
-    echo "Nettoyage des anciennes générations, conservation des 8 dernières..."
+    echo "cleanup old generations..."
     sudo nix-collect-garbage --delete-older-than 30d
     sudo nix-env --delete-generations +8
 }
@@ -45,4 +45,4 @@ replace_hardware_config
 apply_flake
 cleanup_old_generations
 
-echo "Installation terminée !"
+echo "Done!"
